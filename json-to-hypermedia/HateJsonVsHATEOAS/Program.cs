a07eebd1facb16801fc5724542159f4e2e9ad9c5
dpraimeyuu <dam.plaza@gmail.com>
2025-11-23 22:21:00 +0100

    feature: basic interactions

--- json-to-hypermedia/HateJsonVsHATEOAS/Program.cs
+++ json-to-hypermedia/HateJsonVsHATEOAS/Program.cs
@@ -2,5 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Content(File.ReadAllText("index.html"), "text/html"));
-
+app.MapGet("/current-time", () => Results.Json(new
+{
+    time = DateTime.UtcNow,
+    _links = new
+    {
+        reload = new { href = "/" }
+    }
+}));
 app.Run();
