0ec8f361641280f285d560829588cdc3e5da4950
dpraimeyuu <dam.plaza@gmail.com>
2025-11-25 21:36:48 +0100

    feature(json-over-the-wire): introduce refreshing limit

--- json-to-hypermedia/HateJsonVsHATEOAS/Program.cs
+++ json-to-hypermedia/HateJsonVsHATEOAS/Program.cs
@@ -2,14 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Content(File.ReadAllText("index.html"), "text/html"));
+var refreshingSessionsUsage = 0;
 app.MapGet("/current-time", () => Results.Json(new
 {
     time = DateTime.UtcNow,
+    refreshingSessionsLimitReached = refreshingSessionsUsage >= 5,
     _links = new
     {
         reload = new { href = "/" }
     }
 }));
+app.MapPut("/time/refreshing", () =>
+{
+    refreshingSessionsUsage++;
+});
 
 app.MapGet("/hypermedia", () => Results.Content("""
     <h1>HATE JSON Example</h1>
